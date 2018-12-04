349b50b8ae05ce688c439d64f5c0a2d3ee09746b
jesusg1012 <jesus.jg.medina@gmailcom>
2018-12-04 19:29:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |x|
+    if x.start_with?("r") == false
+      return false
+    end
+  end
+    return true
+end
+def contain_a(array)
+  array.select do |x|
+    if x.match("a")
+      x
+    end
+  end
+end
+def first_wa(array)
+ array.each do |x|
+   if x.class == String
+     if x.start_with?("wa")
+     return x
+   end
+ end
+ end
+end
+def remove_non_strings(array)
+  array.select do |x|
+    x.class == String
+  end
+end
+def count_elements(array)
+arr2 = []
+array.each do |x|
+  if !arr2.any?(x)
+    int = array.count(x)
+    arr2.push([int, x])
+  end
+end
+return arr2
+end
